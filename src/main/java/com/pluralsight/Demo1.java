ed3788da26209be712355e8b694c0fe5a17fcfb5
SidiqiObaid <obaid.yearuo@gmail.com>
2024-09-26 16:01:54 -0400

    watched tv

--- src/main/java/com/pluralsight/Demo1.java
+++ src/main/java/com/pluralsight/Demo1.java
@@ -3,5 +3,32 @@ package com.pluralsight;
 public class Demo1 {
     public static void main(String[] args) {
         System.out.println("Hey Class");
+
+        String name = "Oabid";
+        String greetings = "Hello" + name;
+        System.out.println("Hello World");
+
+        char status = 'm';
+        int identifier = 1;
+
+
+        String greeting = "Hello " + name;
+
+
+        int numberOfDaysPerWeekWatchesTV = 5;
+        int minutesPerDayOnDayWatchedOnAverage = 70;
+
+        int minutesPerWeek = numberOfDaysPerWeekWatchesTV * minutesPerDayOnDayWatchedOnAverage;
+        int minutesPerYear = minutesPerWeek * 52;
+
+        int hoursPerYear = minutesPerYear / 60;
+
+        int hoursInWorkWeek = 35;
+
+        int workWeeksPerYearWatchingTV = hoursPerYear / hoursInWorkWeek;
+
+        System.out.println("If " + name + " did not watch TV, it would be like a " + workWeeksPerYearWatchingTV + " week vacation every year");
+        System.out.println(greeting);
+
     }
 }
